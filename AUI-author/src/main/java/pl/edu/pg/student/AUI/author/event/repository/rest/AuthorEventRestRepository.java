e97a0ece1a0c68ebe493ea7c7d6c195137b02e38
JJayJohnny <jasiek.barczewski@gmail.com>
2024-01-05 15:28:06 +0100

    added eureka clients to services

--- AUI-author/src/main/java/pl/edu/pg/student/AUI/author/event/repository/rest/AuthorEventRestRepository.java
+++ AUI-author/src/main/java/pl/edu/pg/student/AUI/author/event/repository/rest/AuthorEventRestRepository.java
@@ -1,6 +1,7 @@
 package pl.edu.pg.student.AUI.author.event.repository.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
 import pl.edu.pg.student.AUI.author.event.repository.api.AuthorEventRepository;
@@ -11,18 +12,23 @@ import java.util.UUID;
 public class AuthorEventRestRepository implements AuthorEventRepository {
     private final RestTemplate restTemplate;
 
+    private final LoadBalancerClient loadBalancerClient;
+
     @Autowired
-    public AuthorEventRestRepository(RestTemplate restTemplate){
+    public AuthorEventRestRepository(RestTemplate restTemplate, LoadBalancerClient loadBalancerClient){
         this.restTemplate = restTemplate;
+        this.loadBalancerClient = loadBalancerClient;
     }
 
     @Override
     public void add(UUID id){
-        restTemplate.put("/api/authors/{id}", null, id);
+        String uri = loadBalancerClient.choose("aui-book").getUri().toString();
+        restTemplate.put(uri + "/api/authors/{id}", null, id);
     }
 
     @Override
     public void delete(UUID id){
-        restTemplate.delete("/api/authors/{id}", id);
+        String uri = loadBalancerClient.choose("aui-book").getUri().toString();
+        restTemplate.delete(uri + "/api/authors/{id}", id);
     }
 }
--- AUI-gateway/src/main/java/pl/edu/pg/student/AUIgateway/AuiGatewayApplication.java
+++ AUI-gateway/src/main/java/pl/edu/pg/student/AUIgateway/AuiGatewayApplication.java
@@ -3,11 +3,13 @@ package pl.edu.pg.student.AUIgateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuiGatewayApplication {
 
 	public static void main(String[] args) {
@@ -17,8 +19,6 @@ public class AuiGatewayApplication {
 	@Bean
 	public RouteLocator routeLocator(
 			RouteLocatorBuilder builder,
-			@Value("${aui.book.url}") String bookUrl,
-			@Value("${aui.author.url}") String authorUrl,
 			@Value("${aui.gateway.host}") String host
 	){
 		return builder.routes()
@@ -27,14 +27,14 @@ public class AuiGatewayApplication {
 						.and()
 						.path("/api/authors/{uuid}",
 								"/api/authors")
-						.uri(authorUrl))
+						.uri("lb://aui-author"))
 				.route("books", route -> route
 						.host(host)
 						.and()
 						.path("/api/books/{uuid}",
 								"/api/books",
 								"/api/authors/{uuid}/books")
-						.uri(bookUrl))
+						.uri("lb://aui-book"))
 				.build();
 	}
 }
